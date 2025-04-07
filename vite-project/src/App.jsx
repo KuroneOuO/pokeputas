bb51c3493adf89b89788be2ed9137032c8df10d5
DavidH <103000000+KuroneOuO@users.noreply.github.com>
2025-04-07 20:48:04 +0000

    holi

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,24 +1,29 @@
 import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Aleatorios from './componentes/aleatorios'
 import Capturados from './componentes/capturados'
 import Favoritos from './componentes/favoritos'
 import Lista from './componentes/lista'
+import Menu from './componentes/menu'
 import Pokemon from './componentes/pokemon'
 import Usuarios from './componentes/usuarios'
 
 function App() {
 
   return (
-    <>
-    <h1>app</h1>
-    <Aleatorios />
-    <Capturados />
-    <Favoritos />
-    <Lista />
-    <Pokemon />
-    <Usuarios />
-    </>
+    <Router>
+
+      <menu />
+      <Routes>
+      <Route path="/" element={<Lista />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/aleatorios" element={<Aleatorios />} />
+        <Route path="/capturados" element={<Capturados />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+      </Routes>
+    </Router>
+  
   )
 }
 
